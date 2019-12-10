f0523b62b65d16869482b8aa2009f772a9205164
Jessica Ibarra <jess19ibarra@gmail.com>
2019-12-10 17:31:30 -0600

    updates to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
-var app = express("body-parer");
+
+var app = express("");
 
 // Sets initial port
 var PORT = process.env.PORT || 3000;
