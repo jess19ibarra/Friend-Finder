e31371d8d8dbe5e4d0fd4cce5c108b5233c98fe3
Jessica Ibarra <jess19ibarra@gmail.com>
2019-12-10 03:02:09 -0600

    updated public files

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+  
+var friends = [{
+        "name": "Ross Geller",
+        "photo": "https://cbsnews3.cbsistatic.com/hub/i/r/2014/09/17/5bb25a75-a598-4bd1-82da-57d32eed3048/resize/620x465/4a6183222c696208e7ffc5e54b9a56ca/schwimmer-ross.jpg",
+        "scores": [5,1,4,4,5,1,2,5,4,1]
+    },
+    {
+        "name": "Monica Geller",
+        "photo": "https://hips.hearstapps.com/ghk.h-cdn.co/assets/16/24/2048x2583/courteney-cox-2000-friends.jpg?resize=480:*",
+        "scores": [4,2,5,1,3,2,2,1,3,2]
+    },
+    {
+        "name": "Rachel Green",
+        "photo": "https://hips.hearstapps.com/ghk.h-cdn.co/assets/15/23/1433364075-gettyimages-909672.jpg",
+        "scores":[5,2,2,2,4,1,3,2,5,5]
+    },
+    {
+        "name": "Chandler Bing",
+        "photo": "https://i.pinimg.com/originals/7e/5f/5c/7e5f5cee4971ae6002c72d35ed8d46d7.jpg",
+        "scores": [3,3,4,2,2,1,3,2,2,3]
+    },
+    {
+        "name": "Joey Tribbiani",
+        "photo": "http://www.elginpk.com/worsley0910_2/stanley/6.jpg",
+        "scores": [4,3,4,1,5,2,5,3,1,4]
+    },
+    {
+        "name": "Phoebe Buffay",
+        "photo": "https://media.raccweb.com/__sized__/celebrity-images/Screen_Shot_2019-09-14_at_9.00.28_AM-crop-c0-5__0-5-400x400.png",
+        "scores": [3,3,4,1,4,3,5,3,5,4]
+    }
+
+]
+
+module.exports = friends;
